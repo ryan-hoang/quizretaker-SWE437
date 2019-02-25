81e48e918fbc9dda7ed5966511329677ca8d235d
Ryan Hoang <rhoang@gmu.edu>
2019-02-25 18:01:22 -0500

    added files for assignment 3

---
+++ quizretakes/printQuizScheduleFormTests.java
@@ -0,0 +1,15 @@
+package quizretakes;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class QuizTest
+{
+
+@Test
+public void test()
+{
+        quizsched.printQuizScheduleForm();
+        assertEquals(1,1);
+}
+
+}
